e58616635544865cad463bc247a30343e1502991
cg-cnu <sreenivas9alapati@gmail.com>
2017-10-17 11:49:19 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/2767425/31648938-0c6151ee-b32e-11e7-8c2c-25ca36a0c878.png"/>
+  <h1 align="center">Hacker Typer</h1>
+  <h4 align="center">Type like a hollywood hacker!</h4>
+  <br>
+</p>
+
+ ## Demo:
+
+<img src=""></img>
+
+## Inspiration:
+
+* [sublime-hacker-typer](https://github.com/rexxars/sublime-hacker-typer)
+* [hackertyper](http://hackertyper.com/)
+
+### Issue/Feedback:
+
+log them in the [github issues](https://github.com/cg-cnu/vscode-hacker-typer/issues) or hit me on [twitter](https://twitter.com/cgcnu).
+
+### Credits:
+
+<div>Logo made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+
+### Like it?:
+
+⭐ this repo.
---
+++ src/hacker.ts
@@ -0,0 +1,51 @@
+'use strict';
+import * as vscode from 'vscode';
+
+export const activate = (context: vscode.ExtensionContext) => {
+
+    const typer = vscode.commands.registerCommand('hacker.typer', () => {
+        const editor: vscode.TextEditor = vscode.window.activeTextEditor;
+        if (!editor) {
+            vscode.window.showErrorMessage("No file open.");
+            return;
+        }
+        // IDEA: logged by admin @ 2017-10-17 11:41:37
+        // provide delay before starting the typing
+        // get the text
+        const text: string = editor.document.getText()
+        // IDEA: logged by admin @ 2017-10-17 11:38:59
+        // if no text is present should look for file with .hackertyper 
+        // select all the text
+        const selection = new vscode.Selection(
+            new vscode.Position(0, 0),
+            editor.document.lineAt(editor.document.lineCount - 1).range.end
+        )
+        // remove selection 
+        editor.edit( editBuilder => {
+            editBuilder.delete(selection)
+        }).then( () => {
+            // write the text
+            for (let i = 0; i <= text.length; i++) {
+                setTimeout(() => {
+                    editor.edit(editBuilder => {
+                        editBuilder.insert(editor.selection.start, text[i]);
+                    })
+                }, i * 100);
+            }
+        })
+        // TODO: created by admin @ 2017-10-17 11:44:00
+        // Move code to config file
+
+        // IDEA: logged by admin @ 2017-10-17 11:44:27
+        // randomize the speed a little
+
+        // IDEA: logged by admin @ 2017-10-17 11:42:08
+        // provide random delays once in a while
+
+        // FIXME: noticed by admin @ 2017-10-17 11:01:22
+        // should be able to cancel with esc or some other key  
+    });
+
+    context.subscriptions.push(typer);
+
+}
